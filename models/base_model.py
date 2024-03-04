68887574a8913556932e0103a8f9f663aa5e8f6c
mkl-74 <mickael1008@gmail.com>
2024-03-04 22:12:47 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -9,18 +9,19 @@ from datetime import datetime
 class BaseModel:
     """create BaseModel"""
     def __init__(self):
+        """initialized"""
         self.id = str(uuid.uuid4())#ID uniq
         self.created_at = datetime.now()#Create
         self.updated_at = datetime.now()#update
-    
+
     def __str__(self):
         """Print class, ID, Dict"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-        
+
     def save(self):
         """update instance with the date and hour"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         nw_dict = self.__dict__.copy()
         nw_dict['__class__'] = self.__class__.__name__
