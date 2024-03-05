14af41d88ee3457c8ef791942b80a5b1df5418c2
TuyenH <thtuyen199@gmail.com>
2024-03-05 15:23:27 +0000

    create program console 0.0.1

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module Console"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Create class HBNB"""
+    prompt = "(hbnb) "  # set the command prompt
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Called when an empty line + ENTER is entered"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
