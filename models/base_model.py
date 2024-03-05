83dae17f969a248c7978ef9908c816c9ea8816d4
mkl-74 <mickael1008@gmail.com>
2024-03-05 11:10:53 +0000

    fix-comment and space

--- models/base_model.py
+++ models/base_model.py
@@ -11,16 +11,16 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initialized"""
         if kwargs:
-            for key, value in kwargs.items():#check key, value
-                if key != "__class__":#different of__class__
+            for key, value in kwargs.items():  # check key, value
+                if key != "__class__":  # different of__class__
                     if key in ['created_at', 'updated_at']:
-                       setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))#if key is 'created_at' or 'updated_at', we convert the value of the string to object 
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))  # noqa: E501 # if key is 'created_at' or 'updated_at', we convert the value of the string to object
                     else:
-                       setattr(self, key, value)#for other key and value
+                        setattr(self, key, value)  # for other key and value
         else:
-            self.id = str(uuid.uuid4())#ID unique
-            self.created_at = datetime.now()#Create
-            self.updated_at = datetime.now()#update
+            self.id = str(uuid.uuid4())  # ID unique
+            self.created_at = datetime.now()  # Create
+            self.updated_at = datetime.now()  # update
 
     def __str__(self):
         """Print class, ID, Dict"""
