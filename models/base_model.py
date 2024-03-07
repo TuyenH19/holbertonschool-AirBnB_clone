96828ca6a7821907911ee7f1ef0e0d1f2a3f94f5
TuyenH19 <146715589+TuyenH19@users.noreply.github.com>
2024-03-07 14:09:27 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,24 +5,15 @@
 import uuid
 import json
 from models.engine.file_storage import FileStorage
-<<<<<<< HEAD
-from models import storage
-=======
->>>>>>> a9bae9209cae94d4abdf1102cdb53da56f6ac28a
 from datetime import datetime
 from models import storage
 
 
 class BaseModel:
     """create BaseModel"""
-<<<<<<< HEAD
-    tformat = "%Y-%m-%dT%H:%M:%S.%f"
-    __nb_objects = 0
-=======
     __nb_objects = 0
     tformat = "%Y-%m-%dT%H:%M:%S.%f"
     storage = None
->>>>>>> a9bae9209cae94d4abdf1102cdb53da56f6ac28a
 
     def __init__(self, *args, **kwargs):
         """initialized"""
@@ -30,25 +21,15 @@ class BaseModel:
             for key, value in kwargs.items():  # check key, value
                 if key != "__class__":  # different of__class__
                     if key in ['created_at', 'updated_at']:
-<<<<<<< HEAD
-                       setattr(self, key, datetime.strptime(value, BaseModel.tformat))#if key is 'created_at' or 'updated_at', we convert the value of the string to object 
-=======
                         setattr(self, key, datetime.strptime(value, BaseModel.tformat))  # noqa # # if key is 'created_at' or 'updated_at', w
->>>>>>> a9bae9209cae94d4abdf1102cdb53da56f6ac28a
                     else:
                         setattr(self, key, value)  # for other key and value
         else:
             BaseModel.__nb_objects += 1
-<<<<<<< HEAD
-            self.id = str(uuid.uuid4())#ID unique
-            self.created_at = datetime.now()#Create
-            self.updated_at = datetime.now()#update
-=======
             self.id = str(uuid.uuid4())  # ID unique
             self.created_at = datetime.now()  # Create
             self.updated_at = datetime.now()  # update
             storage.new(self)
->>>>>>> a9bae9209cae94d4abdf1102cdb53da56f6ac28a
 
     def __str__(self):
         """Print class, ID, Dict"""
