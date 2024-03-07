1f830e729340d1f7b9fc99a0f7cf1a3a77bd4363
mkl-74 <mickael1008@gmail.com>
2024-03-07 13:29:31 +0000

    user

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """class User"""
     def user(self, *args, **kwars):
             def __init__(self):
                 self.email = ""
@@ -11,4 +12,3 @@ class User(BaseModel):
                 self.first_name = ""
                 self.last_name = ""
                 
-            
