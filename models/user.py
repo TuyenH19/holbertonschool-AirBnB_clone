a382bcdd0b31294d90ad271d986ccd4fd6c933bb
mkl-74 <mickael1008@gmail.com>
2024-03-08 08:30:13 +0000

    user_fix_super

--- models/user.py
+++ models/user.py
@@ -5,9 +5,9 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """class User"""
-    def user(self, *args, **kwars):
-            def __init__(self):
-                self.email = ""
-                self.password = ""
-                self.first_name = ""
-                self.last_name = ""
+    def __init__(self, *args, **kwars):
+        super(). __init__(*args, **kwars)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
